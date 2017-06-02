5c9897fb7696ed30a626dab354263709d6026a45
Cole Swingholm <cole.swingholm@gmail.com>
2017-06-01 23:57:05 -0500

    spacemacs

--- startup.sh
+++ startup.sh
@@ -7,6 +7,7 @@ ln -s "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
 ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/dotfiles/.spacemacs" "$HOME/.spacemacs"
 cd ~ || exit
 rm -f "$HOME/gitconfig" 
 ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
